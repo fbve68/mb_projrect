954d80b3f533625c99d3a971c71a3ca8e88e3e0e
Flavio <fbve68@gmail.com>
2020-10-03 18:33:52 -0300

    adicionando testes

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+class PostsModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='Isto é um teste.')
+
+
+    def test_test_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'Isto é um teste.')
+    
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='Este é outro teste.')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
